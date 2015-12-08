f5be7e81a43d2c35be9220ba8ebffa2bb69dea24
hibreez <4ngus.whitehead@gmail.com>
2015-12-08 11:11:50 +0000

    completed

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ ruby '2.2.3'
 
 gem 'sinatra'
 gem 'shotgun'
+gem 'capybara'
+gem 'selenium-webdriver'
+gem 'rspec-sinatra', '0.1.2'
--- app.rb
+++ app.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
 
 get '/' do
-    '<h1>Hello web!</h1>'
+
 end
 
-get '/secrets' do
-  '<i><h3>ssh!</h3> These are my secrets...</i>'
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample 
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red; width: 1000px;'>
-    <img src='http://bit.ly/1eze8aE' style='width: 1000px;'>
-  </div>"
+get '/cat-form' do
+  erb :form
 end
+
+post '/named-cat' do
+  p params
+  @name = params["name"]
+  erb :index
+end
+
+
+
